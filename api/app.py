985063b76b1bfa379a3da03d7c373f77e7187468
Mille Starup <Mille.Starup@outlook.dk>
2025-12-03 12:18:43 +0100

    security

--- api/app.py
+++ api/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
@@ -5,8 +6,18 @@ import uuid
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask:iri123@mariadb:3306/iridium_db'
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
